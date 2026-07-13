d90802ce998560a663ca5e4653e3c28ffdae07ca
ZenoScripter's <wilgtwilgt@gmail.com>
2026-07-13 17:19:07 +0700

    Add files via upload

---
+++ keydata.js
@@ -0,0 +1,12 @@
+[
+  {
+    "username": "zenoid",
+    "key": "a9a7aefc78d6f0d245ef9b6d9d0e4f8f2c5b4d6f1e6d6e4b8f9f2c4d8e7a6b3",
+    "access": true
+  },
+  {
+    "username": "dyx",
+    "key": "2fc77e1fd1b7bfb77cdd4d8479dce7d40d8abf4f89e5dce09f2d91dba7c2ab41",
+    "access": true
+  }
+]
